16112e4cfeadd74e42c3718f5402c094d6bab660
YuJin Kim <68472545+uz1ns@users.noreply.github.com>
2022-01-29 22:18:59 +0900

    Update background.js
    background property added

--- js/background.js
+++ js/background.js
@@ -1,24 +1,8 @@
-/* 
-const images = ["img01.jpg", "img02.jpg", "img03.jpg"];
-
-const no = Math.floor(Math.random()*3);
-const currentImg = `img/${images[no]}`;
-//document.body.style.backgroundImage = "src='img/img01.jpg'";
-
-const image = document.createElement("img");
-image.src = currentImg;
-document.body.appendChild(image);
-
-//body 태그에 들어갈 이미지 
-//background-image: url('img/img01.jpg')
-*/
-
-//아래부터 재작성
 const images = ["img01.jpg", "img02.jpg", "img03.jpg", "img04.jpg"];
 const no = Math.floor(Math.random()*images.length);
 const currentImg = `img/${images[no]}`;
 
-//index 기준이기 때문에...
 document.body.style.backgroundImage = `url(${currentImg})`;
 document.body.style.height = "100%";
 document.body.style.backgroundSize = "cover";
+document.body.style.backgroundRepeat ="no-repeat";
